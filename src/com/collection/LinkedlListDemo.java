f7cac5608294d6cd45db47c64566fb519d0a22bf
Sunny8975 <rajigaresunny0@gmail.com>
2023-01-08 14:45:59 +0530

    addedd linkedlist demo code

---
+++ src/com/collection/LinkedlListDemo.java
@@ -0,0 +1,16 @@
+package com.collection;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedlListDemo {
+
+	public static void main(String[] args) {
+		
+		List<String> list = new LinkedList<String>();
+		list.add("S");
+		list.add("S");
+		list.add("R");
+		System.out.println(list);
+	}
+}
